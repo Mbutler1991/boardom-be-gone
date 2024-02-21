8020eaa1f16f5a93aefb0d16eace4156a1f123a7
Mbutler1991 <147847873+Mbutler1991@users.noreply.github.com>
2024-02-21 13:51:07 +0000

    created django boardom_be_gone project and added a home app

---
+++ boardom_be_gone/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for boardom_be_gone project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boardom_be_gone.settings')
+
+application = get_wsgi_application()
