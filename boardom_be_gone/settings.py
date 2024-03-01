f33120132a2db7f3ba4f23f20df1f48cd09999d9
Mbutler1991 <147847873+Mbutler1991@users.noreply.github.com>
2024-03-01 22:46:34 +0000

    changing debugging to False

--- boardom_be_gone/settings.py
+++ boardom_be_gone/settings.py
@@ -24,10 +24,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mw2@*43#f03qd8b0wpairgz=6e7sp*=l^-%_x7xq&eh8ef8r&5'
+SECRET_KEY = 'mw2@*43#f03qd8b0wpairgz=6e7sp*=l^-%_x7xq&eh8ef8r&5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', os.environ.get('DJANGO_ALLOWED_HOSTS')]
 
