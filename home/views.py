0d00cc0e634c484fcb1b9965be4ba4af9bd07fb6
mbutler1991 <Mark.Butler@henryscheinone.com>
2024-07-04 13:49:41 +0100

    added basic text for home page and added view to randomise products

--- home/views.py
+++ home/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from products.models import Product
+import random
 
 def home_view(request):
-    return render(request, 'home/home.html')
+    products = list(Product.objects.all())
+    random.shuffle(products)
+    random_products = products[:5]
+    return render(request, 'home/home.html', {'random_products': random_products})
